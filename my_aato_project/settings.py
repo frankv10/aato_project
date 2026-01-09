e8d231e350688ae38fbea75905daa6d8339bcc3c
frank <f.visibile@gmail.com>
2026-01-09 11:15:03 +0100

    fix collegamento gestione_documenti

--- my_aato_project/settings.py
+++ my_aato_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "my_aato_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
